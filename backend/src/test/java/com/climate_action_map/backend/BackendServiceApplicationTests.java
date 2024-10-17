aa9221f3a089178acfa3a12ad20cefd1e1f6163d
Jocelyn Tang <jocelynt8ng@gmail.com>
2024-10-17 19:46:23 -0400

    first commit

---
+++ backend/src/test/java/com/climate_action_map/backend/BackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.climate_action_map.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
