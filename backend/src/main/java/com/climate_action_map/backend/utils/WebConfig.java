09fb60b4f49967243cb7cfb580721eadd59b9a3c
Jocelyn Tang <jocelynt8ng@gmail.com>
2024-10-30 13:12:59 -0400

    added CORS config to backend

---
+++ backend/src/main/java/com/climate_action_map/backend/utils/WebConfig.java
@@ -0,0 +1,34 @@
+package com.climate_action_map.backend.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	
+	//Get variables in ConfigProperties
+	private final ConfigProperties secretsConfig;
+	
+	//Use Autowired to make variables available globally
+	@Autowired
+	public WebConfig(ConfigProperties secretsConfigProperties) {
+		this.secretsConfig = secretsConfigProperties;
+	}
+	
+	//Apply CORS configuration to all routes
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+		
+		//Select frontend url based on environment
+		String frontendUrl = "development".equals(secretsConfig.environment())
+				? secretsConfig.devurl()
+				: secretsConfig.produrl();
+		
+        registry.addMapping("/**")
+                .allowedOrigins(frontendUrl)
+                .allowedMethods("GET", "POST");
+    }
+
+}
