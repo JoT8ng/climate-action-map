fa24a578f2b1bfc4ee48f4ccfd17e35bd8b510c6
Jocelyn Tang <jocelynt8ng@gmail.com>
2024-12-17 20:51:23 -0500

    add mapbox basic map

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  map!: mapboxgl.Map;
+
+  ngOnInit() {
+    this.map = new mapboxgl.Map({
+      accessToken: import.meta.env.NG_APP_MAPBOX_TOKEN,
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-79.377399, 43.700574],
+      zoom: 11,
+    });
+  }
 }
